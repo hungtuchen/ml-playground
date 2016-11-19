584dbb2d13b3db3714ab75ba0ac8a34e05ca2bec
transedward <transedward@gmail.com>
2016-11-19 14:11:24 +0800

    Add Naive Bayes

---
+++ supervised/naive_bayes.py
@@ -0,0 +1,60 @@
+from utils.base_estimator import BaseEstimator
+
+import numpy as np
+
+def softmax(z):
+    # Avoid numerical overflow by removing max
+    e = np.exp(z - np.amax(z, axis=1, keepdims=True))
+    return e / np.sum(e, axis=1, keepdims=True)
+
+class GaussianNaiveBayes(BaseEstimator):
+    """Gaussian Naive Bayes."""
+    # Binary problem.
+    n_classes = 2
+
+    def fit(self, X, y=None):
+        self._setup_input(X, y)
+        # Check target labels
+        assert list(np.unique(y)) == [0, 1]
+
+        # Prepare mean, std, prior for each class
+        self._mean = np.zeros((self.n_classes, self.n_features))
+        self._var = np.zeros((self.n_classes, self.n_features))
+        self._prior = np.zeros((self.n_classes))
+
+        for c in xrange(self.n_classes):
+            # Filter X only in current class
+            X_per_class = X[y == c]
+
+            self._mean[c, :] = np.mean(X_per_class, axis=0)
+            self._var[c, :] = np.var(X_per_class, axis=0)
+            self._prior[c] = len(X_per_class) / float(len(X))
+
+    def _predict(self, X=None):
+        """
+        Return the class that each row in X most likely belongs to.
+        Perform classification on an array of test vectors X.
+        """
+        return np.argmax(self._joint_log_likelihood(X), axis=1)
+
+    def predict_proba(self, X):
+        """Predict probability for each class for each row."""
+        return softmax(self._joint_log_likelihood(X))
+
+    def _joint_log_likelihood(self, X):
+        """
+        The conditional probabilities for each class given an feature value are small.
+        When they are multiplied together they result in very small values,
+        which can lead to floating point underflow.
+        A common fix for this is to apply log function on joint probabilities.
+        """
+        joint_log_likelihood = []
+        for c in xrange(self.n_classes):
+            prior = np.log(self._prior[c])
+            # log of denominator part of gaussian distribution
+            posterior = - 0.5 * np.sum(np.log(2. * np.pi * self._var[c, :]))
+            # log of numerator part of gaussian distribution
+            posterior -= 0.5 * np.sum(((X - self._mean[c, :]) ** 2) /
+                                 (self._var[c, :]), axis=1)
+            joint_log_likelihood.append(prior + posterior)
+        return np.array(joint_log_likelihood).T
---
+++ utils/base_estimator.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+class BaseEstimator(object):
+    X = None
+    y = None
+    y_required = True
+
+    def _setup_input(self, X, y=None):
+        """Ensure inputs to an estimator are in the expected format.
+        Ensures X and y are stored as numpy ndarrays by converting from an
+        array-like object if necessary. Enables estimators to define whether
+        they require a set of y target values or not with y_required, e.g.
+        kmeans clustering requires no target labels and is fit against only X.
+        Parameters
+        ----------
+        X : array-like
+            Feature dataset.
+        y : array-like
+            Target values. By default is required, but if y_required = false
+            then may be omitted.
+        """
+        if not isinstance(X, np.ndarray):
+            X = np.array(X)
+
+        if X.size == 0:
+            raise ValueError('Number of features must be > 0')
+
+        if X.ndim == 1:
+            self.n_samples, self.n_features = 1, X.shape
+        else:
+            self.n_samples, self.n_features = X.shape[0], np.prod(X.shape[1:])
+
+        self.X = X
+
+        if self.y_required:
+            if y is None:
+                raise ValueError('Missed required argument y')
+
+            if not isinstance(y, np.ndarray):
+                y = np.array(y)
+
+            if y.size == 0:
+                raise ValueError('Number of targets must be > 0')
+
+        self.y = y
+
+    def fit(self, X, y=None):
+        self._setup_input(X, y)
+
+    def predict(self, X=None):
+        if not isinstance(X, np.ndarray):
+            X = np.array(X)
+
+        if self.X is not None:
+            return self._predict(X)
+        else:
+            raise ValueError('You must call `fit` before `predict`')
+
+    def _predict(self, X=None):
+        raise NotImplementedError()
