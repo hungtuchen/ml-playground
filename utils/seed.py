2eb7820a36ec641dd2ff00ddf1e64cddc8caae2b
transedward <transedward@gmail.com>
2017-03-01 17:37:35 +0800

    Finish basics of deep_q_learning

---
+++ utils/seed.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+
+def set_global_seeds(i):
+    try:
+        import torch
+    except ImportError:
+        pass
+    else:
+        torch.manual_seed(i)
+    np.random.seed(i)
+    random.seed(i)
